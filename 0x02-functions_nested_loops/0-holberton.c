a7762e68c3fa1786ede289398ed14eaa8a20b61b
brianbryank <brianbryan0125@gmail.com>
2021-08-31 12:46:48 -0700

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main - check the code for ALX School students.
  *
@@ -10,15 +11,15 @@
 
 int main(void)
 {
-  char s[] = "_putchar";
-  int i = 0;
+char s[] = "_putchar";
+int i = 0;
 
-  while (s[i] != '\0')
-    {
-      _putchar(s[i]);
-      i++;
-    }
-  _putchar(10);
+while (s[i] != '\0')
+{
+_putchar(s[i]);
+i++;
+}
+_putchar(10);
 
-  return (0);
+return (0);
 }
