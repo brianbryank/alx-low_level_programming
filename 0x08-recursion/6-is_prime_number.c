9c50d8f5ab48921afd789a14b4ee0a6f54842370
brianbryank <brianbryan0125@gmail.com>
2021-09-15 19:27:44 +0300

    added file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,26 +9,28 @@
 
 int is_prime(int n, int divisor)
 {
-  if (n == divisor)
-    return (1);
-  if (n % divisor == 0)
-    return (0);
-  return (is_prime(n, divisor + 1));
+if (n == divisor)
+return (1);
+if (n % divisor == 0)
+return (0);
+return (is_prime(n, divisor + 1));
 
 }
 
 /**
- * is_prime_number - check if prime
- * @n: number to check
- * Return: 11 if prime, 0 if not
- */
+* is_prime_number - check if prime
+* @n: number to check
+* Return: 1 if prime, 0 if not
+*/
 
+int is_prime_number(int n)
+{
 int divisor = 3;
 
 if (n % 2 == 0 || n < 2)
-  return (0);
-if (n == 2 )
-  return (1);
+return (0);
+if (n == 2)
+return (1);
 
 return (is_prime(n, divisor));
 }
