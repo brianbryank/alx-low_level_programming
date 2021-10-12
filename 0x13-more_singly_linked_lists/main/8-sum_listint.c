a0bb4a96d55bebea7273e1d9d1767da8193f3766
brianbryank <brianbryan0125@gmail.com>
2021-10-13 00:48:53 +0300

    update files

--- 0x13-more_singly_linked_lists/main/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/main/8-sum_listint.c
@@ -1,25 +1,20 @@
 #include "lists.h"
-
 /**
- * sum_listint - a function that returns the sum of all the data (n)
- *              of a list.
- *
- * @head: pointer to the first node of the list
- *
- * Return: the sum of all (n)
+* sum_listint - free list
+* @head: pointer
+*Return: pointer
 */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
-
-	while (head != NULL)
-	{
-		/* add n to sum*/
-		sum += head->n;
-		/*go to the next node*/
-		head = head->next;
-	}
-
-	/*if list will be empty it will return sum = 0*/
-	return (sum);
+int sum = 0;
+if (head == NULL)
+{
+return (0);
+}
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
