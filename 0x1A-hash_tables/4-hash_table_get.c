875d425d4fcc33912b8c4e9c0a6d075855d302c3
brianbryank <brianbryan0125@gmail.com>
2021-12-21 14:51:42 +0300

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 unsigned long int index;
-hash_node_t *node;
+ hash_node_t *node;
 if (!ht || !key)
 return (NULL);
 index = key_index((const unsigned char *)key, ht->size);
